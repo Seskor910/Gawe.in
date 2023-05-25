4e9db255419f42deb57af4f3b45b8dd51a45952a
Seskor910 <rafiarya2002@gmail.com>
2023-05-25 18:25:06 +0700

    Nambah buat regis
    Tapi di postman masih error

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 //setup
 const express = require('express');
+//const bodyParser = require('body-parser');
 const app = express();
+const {Pool} = require('pg');
 const port = 3000;
 
-//end point regis
-app.post('/registration', (req,res) =>{
-    try{
-        const {Email, password, firstName, LastName} = req.body;
-
-        //encrypt password
-        const encryptedPassword = await.bcrypt.hash(password);
-
-        const NewUser = {
-        Email,
-        password: encryptedPassword,
-        firstName,
-        LastName,
-        }
-        res.json(NewUser);
-    }
-    catch(err){
-        res.status(500).json({error: 'error'});
-    }
-    
+//connect to PostgreSQL server
+const pool = new Pool({
+    user: '',
+    host: 'localhost',
+    database: 'Gawe.in',
+    password: '',
+    port: 5432,
 });
 
-app.get('/', (req, res) => {
-    res.send('halo');
+//middleware
+app.use(express.json());
+
+//endpoint for account registration
+app.post('/users', (req,res) => {
+    const {email, password} = req.body;
+
+    //email check
+    if(!email || !password){
+        return res.status(405).json({message: 'Email and password is required'});
+    }
+
+    //query to database
+    pool.query('Insert into users (email,password) values ($1, $2)', [email, password], (err) => {
+        if(err){
+            console.error('Terjadi kesalahan', err);
+            return res.status(505).json({message: 'error when saving'});
+        }
+
+        res.status(200).json({message: 'success'});
+    });
 });
 
 app.listen(port, () => {
